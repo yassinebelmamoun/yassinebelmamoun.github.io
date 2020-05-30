3ed600a365c57fa0a9eb85191dd42d6f08219245
Yassine Belmamoun <belmamoun.yassine@gmail.com>
2020-05-30 12:46:51 +0700

    wip

--- content/first-article.md
+++ content/first-article.md
@@ -3,3 +3,13 @@ Date: 2020-05-30 09:50
 Category: Test
 
 This article is the first article. This is a test.
+
+    :::python
+    print("There will be not line number here")
+
+yes
+
+    #!python
+    print("You can see the line number, right?")
+
+This is really great.
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
 
 AUTHOR = 'Yassine Belmamoun'
 SITENAME = 'Yassine Belmamoun - Personal website'
@@ -20,15 +21,61 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Github', 'http://www.github.com/yassinebelmamoun'),
-
-         ('You can modify those links in your config file', '#'),)
+LINKS = ()
 
 # Social widget
-SOCIAL = (('Github', 'http://www.github.com/yassinebelmamoun'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'http://www.github.com/yassinebelmamoun'),
+          ('rss', '/blog/feeds/all.atom.xml'),
+          )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# --------------------
+# Custom configuration for Flex theme
+# Description: https://github.com/alexandrevicenzi/Flex/wiki/Custom-Settings
+# Example: https://github.com/alexandrevicenzi/Flex/wiki/Configuration-example
+# --------------------
+
+# Default Theme
+THEME = 'themes/flex'
+
+SITETITLE = "Yassine Belmamoun"
+SITESUBTITLE = "Tech entrepreneur & Software engineer"
+SITELOGO = SITEURL + "/images/profile_picture_001.jpeg"
+FAVICON = SITEURL + "/images/favicon.ico"
+
+SITEDESCRIPTION = "Yassine Belmamoun - Software Engineer & Tech Entrepreneur"
+BROWSER_COLOR = "#4285f4"
+ROBOTS = "index, follow"
+HOME_HIDE_TAGS = True
+
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa"
+}
+COPYRIGHT_YEAR = 2020
+
+DATE_FORMATS = {
+    'en': '%B %d, %Y',
+}
+STATIC_PATHS = ['images', 'extra/ads.txt']
+EXTRA_PATH_METADATA = {
+    "extra/custom.css": {"path": "static/custom.css"},
+}
+
+MAIN_MENU = True
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
+HOME_HIDE_TAGS = True
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+
+# all PYGMENTS styles are here: https://github.com/alexandrevicenzi/Flex/tree/master/static/pygments
+PYGMENTS_STYLE = 'manni'
+
+LINKS_IN_NEW_TAB = 'external'
