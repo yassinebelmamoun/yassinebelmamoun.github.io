9243c1ef49bfc1c0adac46dc317c6dcb19291d9c
Yassine Belmamoun <belmamoun.yassine@gmail.com>
2020-05-30 18:17:31 +0700

    wip

--- publishconf.py
+++ publishconf.py
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = "" # ! TODO
+# DISQUS_SITENAME = "" # ! TODO
 GOOGLE_ANALYTICS = "UA-138287199-2"
