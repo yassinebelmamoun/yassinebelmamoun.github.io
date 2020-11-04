8cd354744ea3e491f5e12d0573c5017b487bb275
Yassine Belmamoun <belmamoun.yassine@gmail.com>
2020-11-04 10:49:39 +0700

    Deactivate disqus

--- publishconf.py
+++ publishconf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from pelicanconf import *
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
@@ -8,7 +9,6 @@ from __future__ import unicode_literals
 import os
 import sys
 sys.path.append(os.curdir)
-from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://yassinebelmamoun.com'
@@ -23,5 +23,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "yassinebelmamoun"
+# DISQUS_SITENAME = "yassinebelmamoun"
 GOOGLE_ANALYTICS = "UA-138287199-2"
