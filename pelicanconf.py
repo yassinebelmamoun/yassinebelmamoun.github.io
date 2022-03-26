da10ca84e6c88e901b24db7ec29d5914d212a553
Yassine Belmamoun <belmamoun.yassine@gmail.com>
2022-03-26 17:57:47 +0700

    Update pelican settings and setup

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,15 +3,15 @@
 from __future__ import unicode_literals
 from datetime import datetime
 
-AUTHOR = 'Yassine Belmamoun'
-SITEURL = 'http://localhost:8000'
+AUTHOR = "Yassine Belmamoun"
+SITEURL = "http://localhost:8000"
 
-PATH = 'content'
-OUTPUT_PATH = 'output/'
+PATH = "content"
+OUTPUT_PATH = "output/"
 
-TIMEZONE = 'Asia/Bangkok'
+TIMEZONE = "Asia/Bangkok"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,11 +24,13 @@ AUTHOR_FEED_RSS = None
 LINKS = ()
 
 # Social widget
-SOCIAL = (('github', 'http://www.github.com/yassinebelmamoun'),
-          ('facebook', 'https://www.facebook.com/yassine.belmamoun'),
-          ('envelope', 'mailto:belmamoun.yassine@gmail.com'),
-          ('rss', '/blog/feeds/all.atom.xml'),
-          )
+SOCIAL = (
+    ("linkedin", "https://www.linkedin.com/in/belmamoun-yassine/"),
+    ("github", "http://www.github.com/yassinebelmamoun"),
+    ("facebook", "https://www.facebook.com/yassine.belmamoun"),
+    ("envelope", "mailto:belmamoun.yassine@gmail.com"),
+    ("rss", "/blog/feeds/all.atom.xml"),
+)
 
 DEFAULT_PAGINATION = 10
 
@@ -42,12 +44,16 @@ DEFAULT_PAGINATION = 10
 # --------------------
 
 # Default Theme
-THEME = 'themes/flex'
+THEME = "themes/flex"
+
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ["post_stats"]
+
 
 SITENAME = "Yassine Belmamoun - Personal Website"
 SITETITLE = "Yassine Belmamoun"
 SITESUBTITLE = "Tech entrepreneur & Software engineer"
-SITELOGO = SITEURL + "/images/profile_picture_001.jpeg"
+SITELOGO = SITEURL + "/images/profile_picture_002.jpg"
 FAVICON = SITEURL + "/images/favicon/favicon.ico"
 
 SITEDESCRIPTION = "Yassine Belmamoun - Software Engineer & Tech Entrepreneur"
@@ -58,33 +64,35 @@ HOME_HIDE_TAGS = True
 CC_LICENSE = {
     "name": "Creative Commons Attribution-ShareAlike",
     "version": "4.0",
-    "slug": "by-sa"
+    "slug": "by-sa",
 }
-COPYRIGHT_YEAR = 2020
+COPYRIGHT_YEAR = datetime.now().year
 
 DATE_FORMATS = {
-    'en': '%B %d, %Y',
+    "en": "%B %d, %Y",
 }
 
 
 MAIN_MENU = True
-MENUITEMS = (('Archives', '/archives.html'),
-             ('Categories', '/categories.html'),
-             ('Tags', '/tags.html'),)
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
 
 HOME_HIDE_TAGS = True
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
 
 # all PYGMENTS styles are here: https://github.com/alexandrevicenzi/Flex/tree/master/static/pygments
-PYGMENTS_STYLE = 'manni'
+PYGMENTS_STYLE = "manni"
 
-LINKS_IN_NEW_TAB = 'external'
+LINKS_IN_NEW_TAB = "external"
 USE_FOLDER_AS_CATEGORY = True
 
 # USE_LESS = True
-STATIC_PATHS = ['images', 'extra']
+STATIC_PATHS = ["images", "extra"]
 EXTRA_PATH_METADATA = {
     "extra/custom.css": {"path": "static/custom.css"},
-    'extra/CNAME': {'path': 'CNAME'}
+    "extra/CNAME": {"path": "CNAME"},
 }
-CUSTOM_CSS = 'static/custom.css'
+CUSTOM_CSS = "static/custom.css"
